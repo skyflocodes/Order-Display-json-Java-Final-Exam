204f7042265005fb6208edfda15c1b7511f12188
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2020-12-10 13:24:10 -0500

    product done

--- src/Models/Product.java
+++ src/Models/Product.java
@@ -1,4 +1,64 @@
 package Models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Product {
+  @SerializedName("sku")
+  private String sku;
+  @SerializedName("name")
+  private String name;
+  @SerializedName("salePrice")
+  private double salePrice;
+  @SerializedName("regularPrice")
+  private double regularPrice;
+  @SerializedName("images")
+  private String images;
+
+  public Product(String sku, String name, double salePrice, double regularPrice, String images) {
+    setSku(sku);
+    setName(name);
+    setSalePrice(salePrice);
+    setRegularPrice(regularPrice);
+    setImages(images);
   }
+
+  public String getSku() {
+    return sku;
+  }
+
+  public void setSku(String sku) {
+    this.sku = sku;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getSalePrice() {
+    return salePrice;
+  }
+
+  public void setSalePrice(double salePrice) {
+    this.salePrice = salePrice;
+  }
+
+  public double getRegularPrice() {
+    return regularPrice;
+  }
+
+  public void setRegularPrice(double regularPrice) {
+    this.regularPrice = regularPrice;
+  }
+
+  public String getImages() {
+    return images;
+  }
+
+  public void setImages(String images) {
+    this.images = images;
+  }
+}
